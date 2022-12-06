70caa0dc4154b590475902df8a5bdd722778966b
Sean Shmulevich <seanshmulevich@Seans-MacBook-Pro-3.local>
2022-12-06 12:10:56 -0500

    Key task implemented still need to fix REST bug (home screen not refreshing data after new projects are created)

--- ophelia/lib/inputProject.dart
+++ ophelia/lib/inputProject.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:date_time_picker/date_time_picker.dart';
 import 'package:http/http.dart' as http;
 import 'package:go_router/go_router.dart';
 import 'myappbar.dart';
@@ -16,7 +17,13 @@ class _ProjectInputState extends State<ProjectInput> {
   /// Creates a [Page1Screen].
   final TextEditingController _controller = TextEditingController();
   final FocusNode _focusNode = FocusNode();
-  late Future<Project> myData;
+  final TextEditingController _controller2 = TextEditingController();
+  final FocusNode _focusNode2 = FocusNode();
+  final TextEditingController _controller3 = TextEditingController();
+  final FocusNode _focusNode3 = FocusNode();
+  late Project myData;
+  //stateful piece of information set to the output of the datepicker that i used.
+  var dateInput = null;
 
   @override
   void initState() {
@@ -69,25 +76,34 @@ class _ProjectInputState extends State<ProjectInput> {
                     ),
                   ),
                 ),
-                TextFormField(
-                  focusNode: _focusNode,
-                  controller: _controller,
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: 'Project Deadline',
-                  ),
+                DateTimePicker(
+                  type: DateTimePickerType.date,
+                  dateMask: 'd MMM, yyyy',
+                  initialValue: DateTime.now().toString(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2100),
+                  icon: Icon(Icons.event),
+                  dateLabelText: 'Date',
+                  onChanged: (val) {
+                    dateInput = val;
+                  },
+                  onSaved: (val) {
+                    dateInput = val;
+                  },
                 ),
                 TextFormField(
-                  focusNode: _focusNode,
-                  controller: _controller,
+                  keyboardType: TextInputType.number,
+                  focusNode: _focusNode2,
+                  controller: _controller2,
                   decoration: const InputDecoration(
                     border: UnderlineInputBorder(),
                     labelText: 'Number of sessions to complete',
                   ),
                 ),
                 TextFormField(
-                  focusNode: _focusNode,
-                  controller: _controller,
+                  keyboardType: TextInputType.number,
+                  focusNode: _focusNode3,
+                  controller: _controller3,
                   decoration: const InputDecoration(
                     border: UnderlineInputBorder(),
                     labelText: 'Approximate the number of hours to complete',
@@ -96,9 +112,20 @@ class _ProjectInputState extends State<ProjectInput> {
                 Align(
                   alignment: Alignment.bottomCenter,
                   child: TextButton(
-                    onPressed: () => {
-                      myData = createProject(),
-                      context.go('/generateSchedules/1'),
+                    onPressed: () async {
+                      print(dateInput);
+                      //dont submit or go to the next screen if the user hasnt entered anything.
+                      if (_controller.text == "" ||
+                          _controller2.text == "" ||
+                          dateInput == null ||
+                          _controller3.text == "") {
+                        return;
+                      }
+                      myData = await createProject(_controller.text, dateInput,
+                          _controller2.text, _controller3.text);
+                      var plannedIndex = myData.projectIndex;
+                      //go to the screen with the project that was just planned.
+                      context.go('/generateSchedules/$plannedIndex');
                     },
                     child: Container(
                       width: (double.infinity),
@@ -197,17 +224,18 @@ class ProjectDay {
   }
 }
 
-Future<Project> createProject() async {
+Future<Project> createProject(
+    projectName, projectDeadline, numSessions, numHours) async {
   final response = await http.post(
     Uri.parse('http://71.182.194.216:8080/planProject'),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
     body: jsonEncode(<String, String>{
-      "projectName": "testName",
-      "projectDeadline": "12/25/2022",
-      "numSessions": "5",
-      "numHours": "2"
+      "projectName": "$projectName",
+      "projectDeadline": "$projectDeadline",
+      "numSessions": "$numSessions",
+      "numHours": "$numHours"
     }),
   );
 
@@ -218,6 +246,7 @@ Future<Project> createProject() async {
   } else {
     // If the server did not return a 201 CREATED response,
     // then throw an exception.
+    print("wrong values or the algorithm on the backend is broken.");
     throw Exception('Failed to create project.');
   }
 }
